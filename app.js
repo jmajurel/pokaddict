40bf0667fde007af9b99d468703c80eb78a6de66
j.majurel <j.majurel@bpp-tech.com>
2018-04-22 23:40:56 +0100

    add edit, update routes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var methodOverride = require('method-override');
 var pokemonsRouter = require('./routes/pokemons');
 
 var app = express();
 
-const port = process.env.PORT || 8080
-
+const port = process.env.PORT || 8081
+app.use(methodOverride('_method'));
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
 
@@ -14,6 +14,7 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/pokemons', pokemonsRouter);
+
 app.get('/', function(req, res){
   res.render('welcome');
 });
--- routes/pokemons.js
+++ routes/pokemons.js
@@ -21,7 +21,7 @@ router.get('/', function(req, res){
       }
     })
     .on('error', function(error){
-      console.log(err);
+      console.log(error);
     })
 });
 
@@ -30,8 +30,65 @@ router.get("/new", function(req, res){
   res.render("./pokemons/new");
 });
 
+//Create route
 router.post("/", function(req, res){
-  console.log(req.body.pokemon);
+
+  var pokemon = {
+    name: req.body.name,
+    picture: req.body.picture,
+    type: req.body.type
+   };
+   request.post({
+     uri: apiurl, 
+     form: pokemon
+     })
+     .on('response', function(response){
+       console.log(response.statusCode);
+       if(response.statusCode !== 201){
+         console.log('wrong statusCode!');
+       } else {
+         res.redirect('/pokemons');
+       }
+     })
+     .on('error', function(error){
+       console.log(error);
+     });
+});
+
+//Edit route 
+router.get('/edit/:id', function(req, res){
+  request.get(apiurl + req.params.id)
+    .on('response', function(response){
+      if(response.statusCode !== 200){
+        console.log("wrong statusCode!");
+      } else {
+        response.on('data', function(data){
+          res.render('./pokemons/edit', {pokemon: JSON.parse(data)});
+        })
+      }
+    })
+    .on('error', function(error){
+      console.log(error);
+    })
+});
+
+//Update route
+router.put('/:id', function(req, res){
+  console.log(req.body);
+  request.put({ 
+    uri: apiurl + req.params.id,
+    form: req.body
+    })
+    .on('response', function(response){
+      if(response.statusCode !== 201){
+        console.log("wrong statusCode!");
+      } else {
+        res.redirect('/pokemons');
+      }
+    })
+    .on('error', function(error){
+      console.log(error);
+    });
 });
 
 //Show Route
@@ -44,7 +101,7 @@ router.get('/:id', function(req, res){
       } else {
 	response.on('data', function(data){
 	  var pokemon = JSON.parse(data);
-          res.render('./pokemons/show', {pokemon: pokemon, url: appurl});
+          res.render('./pokemons/show', {pokemon: pokemon});
 	});
       }
     })
